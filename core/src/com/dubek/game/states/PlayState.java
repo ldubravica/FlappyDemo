1a7cbd07450d5ee85def36e9e5d2c3e5c3f9d6bb
ldubravica <luka.dubravica@gmail.com>
2016-06-05 21:34:52 +0200

    Ended 8th Video

--- core/src/com/dubek/game/states/PlayState.java
+++ core/src/com/dubek/game/states/PlayState.java
@@ -1,24 +1,33 @@
 package com.dubek.game.states;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.dubek.game.FlappyDemo;
 import com.dubek.game.sprites.Bird;
+import com.dubek.game.sprites.Tube;
 
 /**
  * Created by Luka on 5.6.2016..
  */
 public class PlayState extends State {
     private Bird bird;
+    private Texture bg;
+    private Tube tube;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 300);
         cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
+        bg = new Texture("bg.png");
+        tube = new Tube(100);
     }
 
     @Override
     protected void handleInput() {
-
+        if(Gdx.input.justTouched()){
+            bird.jump();
+        }
     }
 
     @Override
@@ -31,7 +40,10 @@ public class PlayState extends State {
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
+        sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+        sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
         sb.end();
     }
 
