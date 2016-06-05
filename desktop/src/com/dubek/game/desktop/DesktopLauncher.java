7201d9bf9fec4bd8a19b28a1f37f239632297612
ldubravica <luka.dubravica@gmail.com>
2016-06-05 17:27:08 +0200

    Initial commit #1

---
+++ desktop/src/com/dubek/game/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.dubek.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dubek.game.FlappyDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.width = FlappyDemo.WIDTH;
+		config.height = FlappyDemo.HEIGHT;
+		config.title = FlappyDemo.TITLE;
+
+		new LwjglApplication(new FlappyDemo(), config);
+	}
+}
