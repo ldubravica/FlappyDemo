54e132fdf103238347385b35955a50cedbf82ccd
ldubravica <luka.dubravica@gmail.com>
2016-06-06 22:00:46 +0200

    Ended 10th Video

--- core/src/com/dubek/game/sprites/Tube.java
+++ core/src/com/dubek/game/sprites/Tube.java
@@ -1,6 +1,7 @@
 package com.dubek.game.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 import java.util.Random;
@@ -17,6 +18,7 @@ public class Tube {
 
     private Texture topTube, bottomTube;
     private Vector2 posTopTube, posBotTube;
+    private Rectangle boundsTop, boundsBot;
     private Random rand;
 
     public Tube(float x){
@@ -26,6 +28,9 @@ public class Tube {
 
         posTopTube = new Vector2(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
         posBotTube = new Vector2(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+
+        boundsTop = new Rectangle(posTopTube.x, posTopTube.y, topTube.getWidth(), topTube.getHeight());
+        boundsBot = new Rectangle(posBotTube.x, posBotTube.y, bottomTube.getWidth(), bottomTube.getHeight());
     }
 
     public Texture getTopTube() {
@@ -47,5 +52,12 @@ public class Tube {
     public void reposition(float x){
         posTopTube.set(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
         posBotTube.set(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+
+        boundsTop.setPosition(posTopTube.x, posTopTube.y);
+        boundsBot.setPosition(posBotTube.x, posBotTube.y);
+    }
+
+    public boolean collides(Rectangle player){
+        return player.overlaps(boundsTop) || player.overlaps(boundsBot);
     }
 }
